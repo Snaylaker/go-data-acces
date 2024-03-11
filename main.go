3b9c7cfde933abaa6d9b62ffdcce189b31ab7150
Snaylaker <mehdihimmid73@gmail.com>
2024-03-11 16:11:51 +0100

    feat: added selectByID

--- main.go
+++ main.go
@@ -11,23 +11,34 @@ import (
 var db *sql.DB
 
 type Album struct {
-	Id     int64
+	ID     int64
 	Title  string
 	Artist string
 	Price  float32
+}
+
+func albumByID(id int64) (Album, error) {
+	var alb Album
+	row := db.QueryRow("SELECT * FROM album WHERE id = ?", id)
+	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+		if err == sql.ErrNoRows {
+			return alb, fmt.Errorf("albumsById %d: no such album", id)
+		}
+		return alb, fmt.Errorf("albumsById %d: %v", id, err)
+	}
+	return alb, nil
 }
 
 func albumsByArtist(name string) ([]Album, error) {
 	var albums []Album
 	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
 	if err != nil {
-		fmt.Printf("help")
 		return nil, fmt.Errorf("albumsByArtist  %q: %v", name, err)
 	}
 	defer rows.Close()
 	for rows.Next() {
 		var alb Album
-		if err := rows.Scan(&alb.Id, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
 			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
 		}
 		albums = append(albums, alb)
@@ -57,4 +68,10 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("Albums found: %v\n", albums)
+
+	album, err := albumByID(2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Album found: %v\n", album)
 }
